2635a6a273475cb6971b5919cee0c5e6f514c06a
Matthew Lima <frothiestma2@gmail.com>
2020-07-20 16:07:40 -0400

    Added class prototype for user

---
+++ User.h
@@ -0,0 +1,24 @@
+#pragma once
+#include <string>
+
+class User
+{
+public:
+	// Constructor
+	User(std::string u, int c);
+
+	// Setters
+	void setUserName(std::string newName);
+	void setCategory(int newCat);
+	void setStatus(int newStat);
+	// Getters
+	std::string getUserName();
+	int getCategory();
+	int getStatus();
+
+private:
+	// Data members
+	std::string userName;
+	int category;
+	int status;
+};
